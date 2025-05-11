fcfaa3e51ec311bae2c1a251a6274260a83a3d80
onit-tindwani <onittindwani23@gmail.com>
2025-05-12 00:06:56 +0530

    Initial content

--- example_backend/index.js
+++ example_backend/index.js
@@ -30,7 +30,7 @@ app.post('/create-web-call', async (req, res) => {
             payload,
             {
                 headers: {
-                    'Authorization': 'Bearer YOUR_API_KEY', // Replace with your actual Bearer token
+                    'Authorization': 'Bearer key_25e95be8cefba2d8030bdee8c5fe', // Replace with your actual Bearer token
                     'Content-Type': 'application/json',
                 },
             }
